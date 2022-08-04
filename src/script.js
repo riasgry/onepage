249847681cfe5042e3954b8ca1ce5e477845796c
riasgry <rias.gry123@gmail.com>
2022-08-04 23:26:19 +0700

    Finish Maybe

--- src/script.js
+++ src/script.js
@@ -5,3 +5,17 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $offset.height());
     });
   });
+
+  
+  document.addEventListener('DOMContentLoaded', (e) => {
+    let mql = window.matchMedia('(max-width: 800px)');
+    e.preventDefault()
+    mql.onchange = (e) => {
+      if (e.matches) {
+      console.log('This is a narrow screen — less than 600px wide.')
+    } else {
+      /* the viewport is more than 600 pixels wide */
+      console.log('This is a wide screen — more than 600px wide.')
+    }
+  }
+});
